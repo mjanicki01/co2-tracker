328f932451571e2277154e936f08a229d84e13d2
Magda Janicki <janickimagda@gmail.com>
2022-03-27 17:12:51 -0400

    env variables

--- config.py
+++ config.py
@@ -13,13 +13,13 @@ SECRET_KEY = environ.get('SECRET_KEY')
 STATIC_FOLDER = 'static'
 TEMPLATES_FOLDER = 'templates'
 
-FLASK_ENV = 'development'
+FLASK_ENV = 'production'
 TESTING = True
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 DEBUG = True
 
 # Database
-SQLALCHEMY_DATABASE_URI = "postgresql:///co2budget"
+SQLALCHEMY_DATABASE_URI = "postgres://oalakyzygocekd:094c598c123f898c5f4f4c312d1bcff1d61faaf956fd115b79a449f6e82dad35@ec2-54-160-109-68.compute-1.amazonaws.com:5432/d42efu0qie1guq"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = True
 
