8c5784f629d675e4ab79d1c7c0146b4043ef52b3
Magda Janicki <janickimagda@gmail.com>
2022-03-27 20:08:47 -0400

    postgresql

--- config.py
+++ config.py
@@ -19,6 +19,6 @@ DEBUG_TB_INTERCEPT_REDIRECTS = False
 DEBUG = True
 
 # Database
-SQLALCHEMY_DATABASE_URI = "postgres://oalakyzygocekd:094c598c123f898c5f4f4c312d1bcff1d61faaf956fd115b79a449f6e82dad35@ec2-54-160-109-68.compute-1.amazonaws.com:5432/d42efu0qie1guq"
+SQLALCHEMY_DATABASE_URI = "postgresql://oalakyzygocekd:094c598c123f898c5f4f4c312d1bcff1d61faaf956fd115b79a449f6e82dad35@ec2-54-160-109-68.compute-1.amazonaws.com:5432/d42efu0qie1guq"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = True
