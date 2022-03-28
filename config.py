7c984646228fedf150468a28b3eb34b182328729
Magda Janicki <janickimagda@gmail.com>
2022-03-27 20:06:44 -0400

    switch flask_env var

--- config.py
+++ config.py
@@ -13,49 +13,12 @@ SECRET_KEY = environ.get('SECRET_KEY')
 STATIC_FOLDER = 'static'
 TEMPLATES_FOLDER = 'templates'
 
-FLASK_ENV = 'production'
+FLASK_ENV = 'development'
 TESTING = True
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 DEBUG = True
 
 # Database
-SQLALCHEMY_DATABASE_URI = "postgresql://oalakyzygocekd:094c598c123f898c5f4f4c312d1bcff1d61faaf956fd115b79a449f6e82dad35@ec2-54-160-109-68.compute-1.amazonaws.com:5432/d42efu0qie1guq"
+SQLALCHEMY_DATABASE_URI = "postgres://oalakyzygocekd:094c598c123f898c5f4f4c312d1bcff1d61faaf956fd115b79a449f6e82dad35@ec2-54-160-109-68.compute-1.amazonaws.com:5432/d42efu0qie1guq"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = True
-
-
-
-
-
-# from os import environ, path
-# from dotenv import load_dotenv
-
-
-# basedir = path.abspath(path.dirname(__file__))
-# load_dotenv(path.join(basedir, '.env'))
-
-# class Config:
-#     """Base config."""
-# # SECRET_KEY = environ.get('SECRET_KEY')
-# STATIC_FOLDER = 'static'
-# TEMPLATES_FOLDER = 'templates'
-
-
-# class ProdConfig(Config):
-#     FLASK_ENV = 'production'
-#     DEBUG = False
-#     TESTING = False
-#     DATABASE_URI = environ.get('PROD_DATABASE_URI')
-    
-
-# class DevConfig(Config):
-#     FLASK_ENV = 'development'
-#     DEBUG = True
-#     TESTING = True
-#     # DATABASE_URI = environ.get('DEV_DATABASE_URI')
-#     DEBUG_TB_INTERCEPT_REDIRECTS = False
-
-#     # Database
-#     SQLALCHEMY_DATABASE_URI = "postgresql:///co2budget"
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-#     SQLALCHEMY_ECHO = True
