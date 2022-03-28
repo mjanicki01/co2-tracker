37c6a80e8bcf257cbfeb04734d04132247fb9f54
Magda Janicki <janickimagda@gmail.com>
2022-03-27 20:10:01 -0400

    switch debug toolbar off

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ TEMPLATES_FOLDER = 'templates'
 FLASK_ENV = 'development'
 TESTING = True
 DEBUG_TB_INTERCEPT_REDIRECTS = False
-DEBUG = True
+DEBUG = False
 
 # Database
 SQLALCHEMY_DATABASE_URI = "postgresql://oalakyzygocekd:094c598c123f898c5f4f4c312d1bcff1d61faaf956fd115b79a449f6e82dad35@ec2-54-160-109-68.compute-1.amazonaws.com:5432/d42efu0qie1guq"
